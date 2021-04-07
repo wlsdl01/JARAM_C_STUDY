5693a15c453ef2ceddf04acd5b78776251296343
Erica-ict <35024187+wlsdl01@users.noreply.github.com>
2021-04-07 20:07:46 +0900

    Update 1.c

--- 1.c
+++ 1.c
@@ -7,6 +7,12 @@
 
 #include <stdio.h>
 
-int main()
-{
+int main() {
+	int arr[10];
+	int i;
+	scanf("%d %d %d %d %d %d %d %d %d %d", arr, arr + 1, arr + 2, arr + 3, arr + 4, arr + 5, arr + 6, arr + 7, arr + 8, arr + 9);
+
+	for (i = 0; i < 10; i++) {
+		printf("%d->", &arr[i]);
+	}
 }
